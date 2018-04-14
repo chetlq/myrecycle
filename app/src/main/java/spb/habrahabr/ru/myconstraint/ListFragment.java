d2fe9dbcb4f6c510e7ba1889320768adac265cbc
Кривой Иван <IKrivoy.SBT@sberbank.ru>
2018-04-14 18:51:52 +0300

    123

--- app/src/main/java/spb/habrahabr/ru/myconstraint/ListFragment.java
+++ app/src/main/java/spb/habrahabr/ru/myconstraint/ListFragment.java
@@ -3,6 +3,7 @@ package spb.habrahabr.ru.myconstraint;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,7 +21,7 @@ public class ListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_crime_list, container,
                 false);
         mCrimeRecyclerView = (RecyclerView) view
-                .findViewById(R.id.crime_recycler_view);
+                .findViewById(R.id.my_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager
                 (getActivity()));
         return view;

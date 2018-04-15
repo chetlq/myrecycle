59136f6ad290c31406987b7cec66f062d7375322
Кривой Иван <IKrivoy.SBT@sberbank.ru>
2018-04-15 20:13:11 +0300

    холдер работает

--- app/src/main/java/spb/habrahabr/ru/myconstraint/DemoAdapter.java
+++ app/src/main/java/spb/habrahabr/ru/myconstraint/DemoAdapter.java
@@ -1,23 +1,24 @@
 package spb.habrahabr.ru.myconstraint;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
  * Created by admin on 15.04.2018.
  */
 
-public class DemoAdapter extends RecyclerView.Adapter<DemoHolder>{
+public class DemoAdapter extends RecyclerView.Adapter<DemoAdapter.MyHolder>{
     @Override
-    public DemoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(DemoHolder holder, int position) {
+    public void onBindViewHolder(MyHolder holder, int position) {
 
     }
-
 
 
     @Override
@@ -26,4 +27,15 @@ public class DemoAdapter extends RecyclerView.Adapter<DemoHolder>{
     }
 
 
+
+    public class MyHolder extends DemoHolder {
+        public MyHolder(View itemView, @NonNull MyListener onMyListener) {
+            super( itemView, onMyListener );
+        }
+
+        @Override
+        public void bind(@NonNull DemoItem demoItem) {
+            super.bind( demoItem );
+        }
+    }
 }
